c51af0c777049acdeecdbe3e71650e94a297eb7a
Izzanauzi <izzanauzi17@gmail.com>
2026-05-21 23:40:57 +0700

    initial commit

---
+++ bidkita-backend/src/main/java/com/bidkita/bidkita_backend/BidkitaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bidkita.bidkita_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidkitaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidkitaBackendApplication.class, args);
+	}
+
+}
